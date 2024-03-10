dcf7d36145944f46f7cd6717e17747ee43315674
Alexandra <alexandra.nedomolkina@office.e2e4gu.ru>
2024-03-10 23:19:24 +0700

    add age prediction block

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
 import CatFactBlock from './catFactBlock/CatFactBlock';
+import AgePredictionBlock from './agePredictionBlock/AgePredictionBlock';
 
 function App() {
   return (
     <div className="App">
       <CatFactBlock />
+      <AgePredictionBlock />
     </div>
   );
 }
---
+++ src/agePredictionBlock/AgePredictionBlock.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+
+const AgePredictionBlock: React.FC = () => {
+    const [name, setName] = useState('');
+    const { data: age, isLoading, refetch } = useQuery({
+        queryKey: ['agePrediction', name],
+        queryFn: async () => fetch(`https://api.agify.io/?name=` + name).then((res) => res.json())
+    });
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setName(event.target.value);
+    };
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        refetch();
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input type="text" value={name} onChange={handleChange} />
+            <button type="submit" disabled={isLoading}>
+                {isLoading ? 'Loading...' : 'Predict Age'}
+            </button>
+            {age && <div>Age Prediction: {age.age}</div>}
+        </form>
+    );
+}
+
+export default AgePredictionBlock;
